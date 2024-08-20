9ba58b0e7bf19fec98548ebafee89d2bc44c5d26
Fatih Şahin <damfat2307@gmail.com>
2024-08-20 23:07:07 +0300

    Add project files.

---
+++ Controllers/AboutController.cs
@@ -0,0 +1,41 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjee.Controllers
+{
+    public class AboutController : Controller
+    {
+        // GET: About
+
+        AboutManager abm = new AboutManager(new EfAboutDal());//Constructor'a bağlı olarak bir parametre alması gerekiyor.
+        public ActionResult Index()
+        {
+            var aboutvalues = abm.GetList();
+            return View(aboutvalues);
+        }
+
+        [HttpGet]
+        public ActionResult AddAbout()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddAbout(About p)
+        {
+            abm.AboutAdd(p);
+            return RedirectToAction("Index");
+        }
+
+        public PartialViewResult AboutPartial()
+        {
+            return PartialView();
+        }
+    }
+}
---
+++ Controllers/AuthorizationController.cs
@@ -0,0 +1,57 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjee.Controllers
+{
+    public class AuthorizationController : Controller
+    {
+        AdminManager adminManager = new AdminManager(new EfAdminDal());
+        public ActionResult Index()
+        {
+            var adminvalues = adminManager.GetList();
+            return View(adminvalues);
+        }
+
+        [HttpGet]
+        public ActionResult AddAdmin()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddAdmin(Admin p)
+        {
+            adminManager.AdminAdd(p);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult EditAdmin(int id)
+        {
+            var adminvalue = adminManager.GetById(id);
+            return View(adminvalue);
+        }
+
+        [HttpPost]
+        public ActionResult EditAdmin(Admin p)
+        {
+            adminManager.AdminUpdate(p);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult DeleteAdmin(int id)
+        {
+            var deladmin = adminManager.GetById(id);
+            adminManager.AdminDelete(deladmin);
+            return RedirectToAction("Index");
+        }
+    }
+}
