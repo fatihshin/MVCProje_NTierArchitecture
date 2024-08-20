d524e2bb4bf1d448996ff09b79e122caf329360d
Fatih Şahin <damfat2307@gmail.com>
2024-08-20 23:12:26 +0300

    Rewrite

---
+++ DataAccessLayer/Abstract/IRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IRepository<T>
+    {
+        List<T> List();
+
+        void Insert(T p);
+
+        T Get(Expression<Func<T, bool>> filter);//Silme işlemi için T değerinde Get isminde method tanımladık.Türü T Yani Entity.
+
+        void Delete(T p);
+
+        void Update(T p);
+
+        List<T> List(Expression<Func<T, bool>> filter);//Şartlı Listeleme Linq yapısında.
+    }
+}
